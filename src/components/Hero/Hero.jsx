75a7c3949af86ef307eb20dbb35e1c2f4e087f85
Anna Samoilova <samoilova0299@gmail.com>
2025-07-14 23:25:07 +0200

    add raw hero section
    commented HomePage modules as a button global class is used there which should be fixed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,67 @@
-import MainTitle from "../MainTitle/MainTitle";
-import Subtitle from "../Subtitle/Subtitle";
+// import { useNavigate } from 'react-router-dom';
+// import MainTitle from '../MainTitle/MainTitle';
+// import Subtitle from '../Subtitle/Subtitle';
 
-const Hero = ({ onAddRecipeClick }) => (
-  <section>
-    <MainTitle>MainTitle Hero</MainTitle>
-    <Subtitle>Subtitle Hero</Subtitle>
-    <button type="button" onClick={onAddRecipeClick}>
-      Add recipe
-    </button>
-  </section>
-);
+import styles from './Hero.module.css';
+import img from '../../assets/img/hero';
+
+// import { useState } from 'react';
+
+const Hero = () => {
+  // const navigate = useNavigate();
+  // const isLoggedIn = true;
+  // const [showModal, setShowModal] = useState(false);
+  // const toggleModal = () => {
+  //   setShowModal(!showModal);
+  // };
+
+  // const handleAddRecipeButton = () => {
+  //   if (!isLoggedIn) {
+  //     setShowModal(true);
+  //     return;
+  //   }
+  //   navigate('recipe/add');
+  // };
+
+  return (
+    <>
+      {/* {showModal && <Modal onClose={toggleModal} />} */}
+      <section className={styles['hero-section']}>
+        <h1 className={styles.title}>
+          Improve Your
+          <br />
+          Culinary Talents
+        </h1>
+        <p className={styles.desc}>
+          Amazing recipes for beginners in the world of cooking, enveloping you
+          in the aromas and tastes of various cuisines.
+        </p>
+        <button className={styles['hero-button']}>Add Recipe</button>
+        <div className={styles['hero-wrapper-img']}>
+          <div className={styles['hero-img-small']}>
+            <img
+              width="128"
+              src={img.imageSmall1x}
+              srcSet={`${img.imageSmall1x} 1x,
+                                    ${img.imageSmall2x} 2x,
+                                    ${img.imageSmall2x} 3x`}
+              alt="food"
+            />
+          </div>
+          <div className={styles['hero-img-big']}>
+            <img
+              width="302"
+              src={img.imageBig1x}
+              srcSet={`${img.imageBig1x} 1x,
+                                    ${img.imageBig2x} 2x,
+                                    ${img.imageBig2x} 3x`}
+              alt="food"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Hero;
