ea3db9076da708dacb37b064de58b849f94fd2c0
Victoria Stovba <vstovba.00@gmail.com>
2025-07-16 17:33:47 +0400

    feat: add footer component, SVG sprite, and Figma color variables

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,24 @@
+import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo";
 import NetworkLinks from "../NetworkLinks/NetworkLinks";
+import styles from "./Footer.module.css";
 
 const Footer = () => (
-  <footer>
-    <Logo />
-    <NetworkLinks />
-    <p>© {new Date().getFullYear()} Foodies. All rights reserved.</p>
+  <footer className={styles.footer}>
+    <div className={styles.inner}>
+      {}
+      <Link to="/" className={styles.logoFooter}>
+        <Logo />
+      </Link>
+
+      <NetworkLinks className={styles.networkLinks} />
+    </div>
+
+    <hr className={styles.divider} />
+
+    <p className={styles.copy}>
+      © {new Date().getFullYear()}, Foodies. All rights reserved
+    </p>
   </footer>
 );
 
