f31fdb25aa810c84ea3eb7fe3d75aa971a914449
Andrii Diachenko <andreyzp87@gmail.com>
2025-07-17 16:26:29 +0200

    implement basic recipes list styling

--- src/components/Recipes/components/RecipeCard/RecipeCard.jsx
+++ src/components/Recipes/components/RecipeCard/RecipeCard.jsx
@@ -9,41 +9,43 @@ const RecipeCard = ({
 }) => (
   <div className={css.card}>
     <img src={recipe.thumb} alt={recipe.title} className={css.image} />
-    <h3>{recipe.title}</h3>
+    <h3 className={css.title}>{recipe.title}</h3>
     <p>{recipe.description}</p>
-    <button type="button" onClick={onAuthorClick} className={css.authorBtn}>
-      <img
-        src={recipe.owner.avatar}
-        alt={recipe.owner.name}
-        className={css.authorAvatar}
-      />
-      <span>{recipe.owner.name}</span>
-    </button>
-    <div className={css.actions}>
-      <button
-        type="button"
-        onClick={onFavoriteToggle}
-        title="Add/remove from favorites"
-        className={css.favoriteBtn}
-      >
-        <span
-          className={
-            isFavorite
-              ? `${css.favoriteIcon} ${css.favoriteIconActive}`
-              : css.favoriteIcon
-          }
+    <div className={css.footer}>
+      <button type="button" onClick={onAuthorClick} className={css.authorBtn}>
+        <img
+          src={recipe.owner.avatar}
+          alt={recipe.owner.name}
+          className={css.authorAvatar}
+        />
+        <span>{recipe.owner.name}</span>
+      </button>
+      <div className={css.actions}>
+        <button
+          type="button"
+          onClick={onFavoriteToggle}
+          title="Add/remove from favorites"
+          className={css.favoriteBtn}
         >
-          {isFavorite ? "❤️" : "🤍"}
-        </span>
-      </button>
-      <button
-        type="button"
-        onClick={onDetailsClick}
-        title="Recipe details"
-        className={css.detailsBtn}
-      >
-        ➡️
-      </button>
+          <span
+            className={
+              isFavorite
+                ? `${css.favoriteIcon} ${css.favoriteIconActive}`
+                : css.favoriteIcon
+            }
+          >
+            {isFavorite ? "❤️" : "🤍"}
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={onDetailsClick}
+          title="Recipe details"
+          className={css.detailsBtn}
+        >
+          ➡️
+        </button>
+      </div>
     </div>
   </div>
 );
--- src/components/Recipes/components/Recipes/Recipes.jsx
+++ src/components/Recipes/components/Recipes/Recipes.jsx
@@ -1,5 +1,3 @@
-import MainTitle from "../../../MainTitle/MainTitle";
-import Subtitle from "../../../Subtitle/Subtitle";
 import RecipeFilters from "../RecipeFilters/RecipeFilters";
 import RecipeList from "../RecipeList/RecipeList";
 import RecipePagination from "../RecipePagination/RecipePagination";
@@ -9,7 +7,8 @@ import { useNavigate } from 'react-router-dom'
 import { selectIngredients } from '@/redux/slices/ingredientsSlice.js'
 import { selectAreas } from '@/redux/slices/areasSlice.js'
 import { fetchRecipes } from '@/redux/ops/recipesOps';
-import { selectRecipes, selectRecipesPage, selectRecipesPages, selectRecipesIsLoading, selectRecipesError } from '@/redux/slices/recipesSlice';
+import { selectRecipes, selectRecipesIsLoading, selectRecipesError } from '@/redux/slices/recipesSlice';
+import css from "./Recipes.module.css";
 
 const Recipes = ({ category, onBack }) => {
   const dispatch = useDispatch();
@@ -18,8 +17,6 @@ const Recipes = ({ category, onBack }) => {
   const areas = useSelector(selectAreas);
 
   const recipes = useSelector(selectRecipes);
-  const responsePage = useSelector(selectRecipesPage);
-  const totalPages = useSelector(selectRecipesPages);
   const isLoading = useSelector(selectRecipesIsLoading);
   const error = useSelector(selectRecipesError);
 
@@ -43,7 +40,6 @@ const Recipes = ({ category, onBack }) => {
   };
 
   const handlePageChange = (newPage) => {
-    if (newPage < 1 || newPage > totalPages) return;
     setPage(newPage);
     // window.scrollTo({ top: 0, behavior: 'smooth' });
   };
@@ -67,37 +63,37 @@ const Recipes = ({ category, onBack }) => {
   };
 
   return (
-    <section>
-      <button type="button" onClick={onBack} style={{ marginBottom: 16 }}>
-        ← Back
-      </button>
-      <MainTitle>Recipes{category ? `: ${category}` : ''}</MainTitle>
-      <Subtitle>Select filters to search for recipes</Subtitle>
-      <RecipeFilters
-        ingredients={ingredients}
-        regions={areas}
-        selectedIngredient={selectedIngredient}
-        selectedRegion={selectedRegion}
-        onChange={handleFiltersChange}
-      />
-      {isLoading && <div>Loading...</div>}
-      {error && <div style={{ color: 'red' }}>{error}</div>}
-      {!isLoading && !error && (
-        <RecipeList
-          recipes={recipes}
-          favorites={favorites}
-          onAuthorClick={handleAuthorClick}
-          onFavoriteToggle={handleFavoriteToggle}
-          onDetailsClick={handleDetailsClick}
+    <section className={css.recipesSection}>
+      <div className={css.titleWrapper}>
+        <button type="button" onClick={onBack} className={css.backButton}>
+          ← Back
+        </button>
+        <h2 className={css.categoryTitle}>{category.name}</h2>
+        <p className={css.categoryDescription}>{category.desc}</p>
+      </div>
+      <div className={css.recipesWrapper}>
+        <RecipeFilters
+          ingredients={ingredients}
+          regions={areas}
+          selectedIngredient={selectedIngredient}
+          selectedRegion={selectedRegion}
+          onChange={handleFiltersChange}
         />
-      )}
-      {totalPages > 1 && (
-        <RecipePagination
-          page={responsePage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        />
-      )}
+        <div className={css.recipesListWrapper}>
+          {isLoading && <div className={css.loadingPlaceholder}>Loading...</div>}
+          {error && <div className={css.errorMessage}>{error}</div>}
+          {!isLoading && !error && (
+            <RecipeList
+              recipes={recipes}
+              favorites={favorites}
+              onAuthorClick={handleAuthorClick}
+              onFavoriteToggle={handleFavoriteToggle}
+              onDetailsClick={handleDetailsClick}
+            />
+          )}
+          <RecipePagination onPageChange={handlePageChange} />
+        </div>
+      </div>
     </section>
   );
 };
