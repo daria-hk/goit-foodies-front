c84ae77114d946ed21c565c8cce0149399fc5a55
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2025-07-17 18:14:17 +0300

    ADDED: recipe logic to recipesSlice

--- src/redux/slices/recipesSlice.js
+++ src/redux/slices/recipesSlice.js
@@ -1,8 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchRecipes } from "../ops/recipesOps";
+import {
+  fetchRecipes,
+  fetchRecipeById,
+  addRecipeToFavorites,
+  removeRecipeFromFavorites,
+  fetchFavoriteRecipes,
+} from "../ops/recipesOps";
 
 const initialState = {
   items: [],
+  currentRecipe: null,
+  favorites: { recipes: [] },
   isLoading: false,
   error: null,
 };
@@ -25,6 +33,8 @@ export const recipesSlice = createSlice({
       state.items = [];
       state.isLoading = false;
       state.error = null;
+      state.currentRecipe = null;
+      state.favorites.recipes = [];
     },
   },
   extraReducers: (builder) => {
@@ -34,12 +44,35 @@ export const recipesSlice = createSlice({
         state.isLoading = false;
         state.items = action.payload;
       })
-      .addCase(fetchRecipes.rejected, handleRejected);
+      .addCase(fetchRecipes.rejected, handleRejected)
+      .addCase(fetchRecipeById.pending, handlePending)
+      .addCase(fetchRecipeById.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.currentRecipe = action.payload;
+      })
+      .addCase(fetchRecipeById.rejected, handleRejected)
+
+      .addCase(addRecipeToFavorites.fulfilled, (state, action) => {
+        state.favorites.recipes.push(action.payload);
+      })
+      .addCase(removeRecipeFromFavorites.fulfilled, (state, action) => {
+        // state.favorites.recipes.filter(
+        //   (recipe) => recipe.id !== action.meta.arg
+        state.favorites.recipes = state.favorites.recipes.filter(
+          (recipe) => recipe.id !== action.meta.arg
+        );
+      })
+      .addCase(fetchFavoriteRecipes.fulfilled, (state, action) => {
+        console.log("Look at this favorites list");
+        state.favorites = action.payload;
+      });
   },
 });
 
 export const { resetRecipesState } = recipesSlice.actions;
 export const recipesReducer = recipesSlice.reducer;
 export const selectRecipes = (state) => state.recipes.items;
+export const selectCurrentRecipe = (state) => state.recipes.currentRecipe;
 export const selectRecipesIsLoading = (state) => state.recipes.isLoading;
 export const selectRecipesError = (state) => state.recipes.error;
+export const selectFavorites = (state) => state.recipes.favorites.recipes;
