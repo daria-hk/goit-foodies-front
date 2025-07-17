6ee8f9808f106260c3ccacf6e46a9503f60c617c
Andrii Diachenko <andreyzp87@gmail.com>
2025-07-17 12:44:20 +0200

    Extract api url to .env variable

--- src/redux/ops/recipesOps.js
+++ src/redux/ops/recipesOps.js
@@ -1,12 +1,11 @@
-import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../utils/api";
 
 export const fetchRecipes = createAsyncThunk(
   "recipes/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const url = `https://test-xe0u.onrender.com/api/recipes`;
-      const response = await axios.get(url);
+      const response = await api.get("recipes");
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -18,9 +17,7 @@ export const fetchRecipeById = createAsyncThunk(
   "recipes/fetchById",
   async (id, thunkAPI) => {
     try {
-      const response = await axios.get(
-        `https://test-xe0u.onrender.com/api/recipes/${id}`
-      );
+      const response = await api.get(`recipes/${id}`);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -29,12 +26,9 @@ export const fetchRecipeById = createAsyncThunk(
 );
 
 export const fetchRecipesPopular = createAsyncThunk(
-  "recipes/fetchPopular",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get(
-        `https://test-xe0u.onrender.com/api/recipes/popular`
-      );
+      const response = await api.get("recipes/popular");
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -46,7 +40,6 @@ export const createRecipe = createAsyncThunk(
   "recipes/createRecipe",
   async (recipeData, thunkAPI) => {
     try {
-      const url = `https://test-xe0u.onrender.com/api/recipes`;
       const formData = new FormData();
       formData.append("title", recipeData.title);
       formData.append("category", recipeData.category);
@@ -56,7 +49,7 @@ export const createRecipe = createAsyncThunk(
       formData.append("thumb", recipeData.thumb);
       formData.append("time", recipeData.time);
       formData.append("ingredients", JSON.stringify(recipeData.ingredients));
-      const response = await axios.post(url, formData, {
+      const response = await api.post("recipes", formData, {
         headers: { "Content-Type": "multipart/form-data" },
       });
       return response.data;
@@ -71,8 +64,7 @@ export const fetchMyRecipes = createAsyncThunk(
   "recipes/fetchMyRecipes",
   async (_, thunkAPI) => {
     try {
-      const url = `https://test-xe0u.onrender.com/api/recipes/my`;
-      const response = await axios.get(url);
+      const response = await api.get('recipes/my');
       return response.data;
     } catch (e) {
       const message = e.response?.data?.message || e.message;
@@ -85,8 +77,7 @@ export const fetchFavoriteRecipes = createAsyncThunk(
   "recipes/fetchFavoriteRecipes",
   async (_, thunkAPI) => {
     try {
-      const url = `https://test-xe0u.onrender.com/api/recipes/favorites`;
-      const response = await axios.get(url);
+      const response = await api.get('recipes/favorites');
       return response.data;
     } catch (e) {
       const message = e.response?.data?.message || e.message;
@@ -99,8 +90,7 @@ export const addRecipeToFavorites = createAsyncThunk(
   "recipes/addRecipeToFavorites",
   async (id, thunkAPI) => {
     try {
-      const url = `https://test-xe0u.onrender.com/api/recipes/${id}/favorite`;
-      const response = await axios.post(url);
+      const response = await api.post(`recipes/${id}/favorite`);
       return response.data;
     } catch (e) {
       const message = e.response?.data?.message || e.message;
@@ -113,8 +103,7 @@ export const removeRecipeFromFavorites = createAsyncThunk(
   "recipes/removeRecipeFromFavorites",
   async (id, thunkAPI) => {
     try {
-      const url = `https://test-xe0u.onrender.com/api/recipes/${id}/favorite`;
-      const response = await axios.delete(url);
+      const response = await api.delete(`recipes/${id}/favorite`);
       return response.data;
     } catch (e) {
       const message = e.response?.data?.message || e.message;
