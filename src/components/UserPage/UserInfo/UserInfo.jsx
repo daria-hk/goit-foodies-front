56151afd42d04fdb8d3ad133b8881b7bf1d8201f
Mykola Kushka <mykola.kushka@gmail.com>
2025-07-17 23:59:10 +0300

    User info added

--- src/components/UserPage/UserInfo/UserInfo.jsx
+++ src/components/UserPage/UserInfo/UserInfo.jsx
@@ -1,33 +1,67 @@
+import { useRef } from "react";
 import styles from "./UserInfo.module.css";
 import defaultAvatar from "../../../assets/img/user/default-avatar.png";
+import sprite from "../../../assets/img/sprite.svg";
 
 export default function UserInfo({ user, isOwnProfile, onAvatarChange }) {
-  if (!user) return null; 
+  const fileInputRef = useRef(null);
+  if (!user) return null;
+
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.avatarWrapper}>
-        <img
-          src={user.avatar || defaultAvatar}
-          alt="User avatar"
-          className={styles.avatar}
-        />
-        {isOwnProfile && (
-          <input
-            type="file"
-            className={styles.avatarInput}
-            onChange={onAvatarChange}
+      <div>
+        <div className={styles.avatarWrapper}>
+          <img
+            src={user.avatar || defaultAvatar}
+            alt="User avatar"
+            className={styles.avatar}
           />
-        )}
+          {isOwnProfile && (
+            <>
+              <button
+                type="button"
+                className={styles.uploadButton}
+                onClick={handleClick}
+                aria-label="Upload avatar"
+              >
+                <svg className={styles.plusIcon}>
+                  <use href={`${sprite}#icon-plus`} />
+                </svg>
+              </button>
+              <input
+                type="file"
+                ref={fileInputRef}
+                className={styles.avatarInput}
+                onChange={onAvatarChange}
+              />
+            </>
+          )}
+        </div>
+
+        <h3 className={styles.userName}>{user.name || "User"}</h3>
       </div>
 
-      <div className={styles.info}>
-        <p>Email: {user.email}</p>
-        <p>Recipes: {user.recipeCount}</p>
-        <p>Favorites: {user.favoritesCount}</p>
-        <p>Followers: {user.followersCount}</p>
-        <p>Following: {user.followingCount}</p>
-      </div>
+      <dl className={styles.info}>
+        <dt>Email:</dt>
+        <dd>{user.email}</dd>
+
+        <dt>Added recipes:</dt>
+        <dd>{user.recipesCount}</dd>
+
+        <dt>Favorites:</dt>
+        <dd>{user.favoritesCount}</dd>
+
+        <dt>Followers:</dt>
+        <dd>{user.followersCount}</dd>
+
+        <dt>Following:</dt>
+        <dd>{user.followingCount}</dd>
+      </dl>
+
     </div>
   );
 }
