542adf2a0d4d89ca90c0ce5457298c18a614a396
Andrii Diachenko <andreyzp87@gmail.com>
2025-07-21 14:05:29 +0200

    Show login form when unauthorised user adds recipe to favorites

--- src/hooks/useFavorites.js
+++ src/hooks/useFavorites.js
@@ -1,15 +1,22 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { selectFavorites } from '@/redux/slices/recipesSlice';
 import { addRecipeToFavorites, removeRecipeFromFavorites } from '@/redux/ops/recipesOps';
+import { openSignInModal, selectUser } from '@/redux/slices/usersSlice.js'
 
 export const useFavorites = () => {
   const dispatch = useDispatch();
   const favorites = useSelector(selectFavorites);
+  const user = useSelector(selectUser);
 
   const isInFavorites = (recipe) =>
-    favorites?.some(fav => parseInt(fav.id) === parseInt(recipe.id));
+    user && favorites?.some(fav => parseInt(fav.id) === parseInt(recipe.id));
 
   const toggleFavorite = (recipe) => {
+    if (!user) {
+      dispatch(openSignInModal());
+      return;
+    }
+
     if (isInFavorites(recipe)) {
       dispatch(removeRecipeFromFavorites(recipe.id));
     } else {
